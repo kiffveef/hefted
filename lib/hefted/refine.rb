197e9927e97fb8d8d2cee8eee7ba3e15f428794b
weathare <fuzuki.close@gmail.com>
2017-03-10 13:39:23 +0900

    delete unuse refine

--- lib/hefted/refine.rb
+++ lib/hefted/refine.rb
@@ -4,19 +4,11 @@ module Hefted
       def to_camel
         self.gsub(/(?:^|_|\s)(.)/) { $1.upcase }
       end
-
-      def to_setter
-        "#{self}="
-      end
     end
 
     refine Symbol do
       def to_camel
         self.to_s.to_camel.to_sym
-      end
-
-      def to_setter
-        self.to_s.to_setter.to_sym
       end
     end
   end
